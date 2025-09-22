bda2961f8fd06bcb42dc35e20d66b1063a1e6e45
justin <justin9490@gmail.com>
2025-09-21 20:40:24 -0500

    chore: revert to TS import style (remove .js extensions) and adjust tsconfig moduleResolution

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import { App } from './App.js';
+import { App } from './App';
 import { Provider } from 'react-redux';
-import { store } from './store/store.js';
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
